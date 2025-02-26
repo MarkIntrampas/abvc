775bf6847577cc9a50a9b3914c56451f0193867d
MarkIntrampas <daytobemillionaire@gmail.com>
2025-02-26 09:51:02 +0800

    Initial commit

--- src/global_component/Nav.js
+++ src/global_component/Nav.js
@@ -1,5 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Correct import for Bootstrap JS and Popper.js
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Correct import for Bootstrap JS and Popper.js
+//import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js'
+
 import { React, Component } from 'react';
 import './Nav.css';
 import navlogo from "./Nav media/LOGO2.png";
